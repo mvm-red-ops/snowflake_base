5a343ee6c5029793fa5fa88e5e7ee5205fd6620c
taylor <tayloryoung@mvmediasales.com>
2021-11-10 14:51:37 -0500

    key change

--- viewership/pluto/stages/main.sql
+++ viewership/pluto/stages/main.sql
@@ -1,5 +1,5 @@
 create or replace stage pluto_viewership url='s3://nosey-snowflake/viewership/Pluto' 
-credentials=(aws_key_id='AKIAUASF4VJ5VLO3CUBJ' aws_secret_key='f9F1CoeIrX/WPrvZDPqAw2Y+8rlOA128HV/bdeMB')
+credentials=(aws_key_id='ENV[key_id]' aws_secret_key='ENV[secret_key')
 file_format = nosey_viewership;
 
 
