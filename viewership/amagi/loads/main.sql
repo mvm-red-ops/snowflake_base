ba568ad6b0ef36670bf31cc7f2bd65edbf504e8e
mvm-red-ops <56323037+mvm-red-ops@users.noreply.github.com>
2022-01-18 14:14:09 -0500

    Update main.sql

--- viewership/amagi/loads/main.sql
+++ viewership/amagi/loads/main.sql
@@ -39,6 +39,51 @@ pattern='.*amagi_q3_21.*' file_format = nosey_viewership
 ON_ERROR=SKIP_FILE;
 
 
+-- Klowd TV
+copy into AMAGI_VIEWERSHIP(view_date,
+channel_name,
+content_id,
+content_name,
+series,
+device,
+country,
+session_count,	
+total_viewership_minutes,
+unique_viewers,
+avg_session_count,
+avg_session_duration_per_session,
+avg_session_duration_per_viewer,
+channel,
+deal_parent, 
+year, 
+quarter) 
+from (select t.$1, t.$2, t.$4, t.$5, t.$6, t.$7, t.$8,
+    to_number(t.$9, 12, 2),
+    to_number(REPLACE(t.$10, ','), 12, 2),
+    to_number(REPLACE(t.$11, ','), 12, 2),
+    to_number(REPLACE(t.$12, ','), 8, 2),
+    to_number(REPLACE(t.$13, ','), 8, 2), 
+    to_number(REPLACE(t.$14, ','), 8, 2), 
+    'Nosey', 12, 2021, 'q3' from @amagi_viewership t) pattern='.*klowd_2021_q3.*' file_format = nosey_viewership 
+ON_ERROR=SKIP_FILE;
+
+--Update channel_id
+UPDATE AMAGI_VIEWERSHIP t1
+SET channel_id = t2.id
+FROM channels t2
+WHERE t1.channel = t2.name;
+
+--Update territory_id
+UPDATE AMAGI_VIEWERSHIP t1
+    SET territory_id = t2.id
+    FROM territories t2
+    WHERE t1.country = t2.name and t1.year = 2021 and t1.quarter = 'q3' and t1.deal_parent = 12;
+
+--update device_id
+UPDATE AMAGI_VIEWERSHIP t1
+    SET device_id = t2.id
+    FROM devices t2
+    WHERE t1.device = t2.name and t1.year = 2021 and t1.quarter = 'q3' and t1.deal_parent = 12;
 
 -- afterwards update 
 territory_id, deal_parent, channel_id, ref_id
