c304a234f5df8a75c074b8182401d6ecb6e82c02
taylor <tayloryoung@mvmediasales.com>
2022-03-30 11:12:04 -0400

    updated pluto load queries

--- Linear/viewership/pluto/loads/main.sql
+++ Linear/viewership/pluto/loads/main.sql
@@ -1,66 +1,33 @@
 -- PLUTO US
 copy into PLUTO_VIEWERSHIP(
-    month,
+    year_month_day,
     series,
     title,
     clip_name,
     total_viewing_minutes,
     sessions,
-    revenue_by_title,
+    revenue,
     deal_parent,
     territory_id,
     year,quarter,filename)
-from (select t.$1, t.$2, t.$3, t.$4, to_number(REPLACE(REPLACE(t.$5, '-', '0'), ','), 12, 0),  to_number(REPLACE(t.$6, ','), 12, 0), to_decimal(REPLACE(REPLACE(t.$7, '$'), ','), 12, 2), 29, 1, 2021, 'q3', 'pluto_us_2021_q3.csv'
-from @pluto_viewership t) pattern='.*pluto_us_2021_q3.*' file_format = nosey_viewership 
+from (select t.$1, t.$2, t.$3, t.$4, to_number(REPLACE(REPLACE(t.$5, '-', '0'), ','), 12, 0),  to_number(REPLACE(t.$6, ','), 12, 0), to_decimal(REPLACE(REPLACE(t.$7, '$'), ','), 12, 2), 29, 1, 2021, 'q4', 'pluto_us_q4_21.csv'
+from @distribution_partners t) pattern='.*pluto_us_q4_21.*' file_format = nosey_viewership 
 ON_ERROR=SKIP_FILE FORCE=TRUE;
-
---- PLUTO US 2021 Q3
-copy into PLUTO_VIEWERSHIP(
-    month,
-    series_name,
-    clip_name,
-    total_viewing_minutes,
-    sessions,
-    revenue_by_title,
-    deal_parent,
-    territory_id,
-    year, quarter, filename)
-from (select t.$1, t.$2, t.$3, to_number(REPLACE(REPLACE(t.$4, '-', '0'), ','), 12, 0),  to_number(REPLACE(t.$5, ','), 12, 0), to_decimal(REPLACE(REPLACE(t.$6, '$'), ','), 12, 2), 29, 1, 2021, 'q3', 'pluto_2021_q3.csv'
-from @pluto_viewership t) pattern='.*pluto_2021_q3.*' file_format = nosey_viewership 
-ON_ERROR=SKIP_FILE FORCE=TRUE;
-
 
 -- PLUTO UK
-copy into PLUTO_VIEWERSHIP( clip_name, total_revenue, month, ref_id, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1, to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2), t.$3, t.$4, 29, 5, 2021, 'q3', 'pluto_uk_2021_q3' from @pluto_viewership t) pattern='.*pluto_uk_2021_q3.*' file_format = nosey_viewership 
+copy into PLUTO_VIEWERSHIP( clip_name, revenue, year_month_day, deal_parent, territory_id, year, quarter, filename) 
+from (select t.$1, to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2), t.$3, 29, 5, 2021, 'q4', 'pluto_uk_q4_21' from @distribution_partners t) pattern='.*pluto_uk_q4_21.*' file_format = nosey_viewership 
 ON_ERROR=CONTINUE FORCE=TRUE;
 
---- PLUTO UK 2021 q3
-copy into PLUTO_VIEWERSHIP(title, total_revenue, month, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1, to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2), t.$3, 29, 5, 2021, 'q3', 'pluto_uk_2021_q3' from @pluto_viewership t) pattern='.*pluto_uk_2021_q3.*' file_format = nosey_viewership 
-ON_ERROR=CONTINUE FORCE=TRUE;
-   
   
--- pluto lam
-copy into PLUTO_VIEWERSHIP( clip_name, month, total_revenue, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1,  t.$2, to_number(REPLACE(REPLACE(REPLACE(t.$3, '- ', 0), '$'), ','), 12,2),  29, 3, 2021, 'q3', 'pluto_lam_2021_q3' from @pluto_viewership t) pattern='.*pluto_lam_2021_q3.*' file_format = nosey_viewership 
+-- pluto latam
+copy into PLUTO_VIEWERSHIP( clip_name,  revenue, year_month_day, deal_parent, territory_id, year, quarter, filename) 
+from (select t.$1,  to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2),  t.$3, 29, 3, 2021, 'q4', 'pluto_latam_q4_21' from @distribution_partners t) pattern='.*pluto_latam_q4_21.*' file_format = nosey_viewership 
 ON_ERROR=CONTINUE FORCE=TRUE;
 
----pluto lam 2021 q3
-copy into PLUTO_VIEWERSHIP(title, month, total_revenue, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1, t.$2, to_number(REPLACE(REPLACE(REPLACE(t.$3, '- ', 0), '$'), ','), 12,2), 29, 3, 2021, 'q3', 'pluto_lam_2021_q3' from @pluto_viewership t) pattern='.*pluto_lam_2021_q3.*' file_format = nosey_viewership 
-ON_ERROR=CONTINUE FORCE=TRUE;
 
 
 -- pluto brazil
-copy into PLUTO_VIEWERSHIP( title, total_revenue, month, ref_id, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1, to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2), t.$3, t.$4, 29, 7, 2021, 'q2', 'pluto_brazil_2021_q2' from @pluto_viewership t) pattern='.*pluto_brazil_2021_q2.*' file_format = nosey_viewership 
+copy into PLUTO_VIEWERSHIP( title, revenue, year_month_day, deal_parent, territory_id, year, quarter, filename) 
+from (select t.$1, to_number(REPLACE(REPLACE(REPLACE(t.$2, '- ', 0), '$'), ','), 12,2), t.$3,  29, 7, 2021, 'q4', 'pluto_brazil_q4_21' from @distribution_partners t) pattern='.*pluto_brazil_q4_21.*' file_format = nosey_viewership 
 ON_ERROR=CONTINUE FORCE=TRUE;
-
----pluto brazil 2021 q3
-copy into PLUTO_VIEWERSHIP( title, month, total_revenue, deal_parent, territory_id, year, quarter, filename) 
-from (select t.$1, t.$2, to_number(REPLACE(REPLACE(REPLACE(t.$3, '- ', 0), '$'), ','), 12,2), 29, 7, 2021, 'q3', 'pluto_brazil_2021_q3' from @pluto_viewership t) pattern='.*pluto_brazil_2021_q3.*' file_format = nosey_viewership 
-ON_ERROR=CONTINUE FORCE=TRUE;
-
-
-
--- Linear/viewership/wurl/loads/samsung.sql
+++ Linear/viewership/wurl/loads/samsung.sql
@@ -1,6 +1,24 @@
 -- WURL Samsung
+-- HAVE TO UPDATE TERRITORY_ID AND FILENAME
+copy into WURL_VIEWERSHIP(date, title, ref_id, occurances,tot_hov, tot_sessions, mov_per_session, tot_completions, vs, deal_parent,territory_id, channel_id, year, quarter, filename) 
+from (select 
+      t.$1, 
+      REPLACE(t.$2, ','),
+      t.$3, 
+      to_number(REPLACE(t.$4, ','), 5, 2), 
+      to_number(REPLACE(t.$5, ','), 20, 5), 
+      to_number(REPLACE(t.$6, ','), 9, 2),  
+      to_number(REPLACE(t.$7, ','), 20, 8), 
+      to_decimal(REPLACE(t.$8, ','), 10, 2),
+      'Samsung', 18, TERRITORY_ID_GOES HERE, 13, 2021, 'q4', 'FILENAME HERE.csv'
+from @distribution_partners t) pattern='.*FILENAME HERE.*' file_format = nosey_viewership 
+ON_ERROR=SKIP_FILE FORCE=TRUE;
+
+
+-- OLD WURL Samsung
 copy into WURL_VIEWERSHIP(title, avg_session_hov, occurances ,tot_completions,tot_hov, tot_sessions,vs,month,ref_id,share,revenue, deal_parent,territory_id, year, quarter, filename) 
 from (select t.$1, 
+t.$2, 
       to_decimal(REPLACE(t.$2, ','), 15, 10), 
       to_number(REPLACE(t.$3, ','), 10, 1), 
       to_number(REPLACE(t.$4, ','), 15, 6),  
@@ -12,6 +30,3 @@ from (select t.$1,
       18, 1 , 2021, 'q2', 'samsung_2021_q2.csv'
 from @wurl_viewership t) pattern='.*samsung_2021_q2.*' file_format = nosey_viewership 
 ON_ERROR=SKIP_FILE FORCE=TRUE;
-
-
-
