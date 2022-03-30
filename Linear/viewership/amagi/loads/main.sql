32f4e1cb77170693d61bf77279e8fc7dbcff25b2
taylor <tayloryoung@mvmediasales.com>
2022-03-30 12:03:49 -0400

    general amagi load update

--- Linear/viewership/amagi/loads/main.sql
+++ Linear/viewership/amagi/loads/main.sql
@@ -1,72 +1,32 @@
--- distrovTv
-copy into AMAGI_VIEWERSHIP(view_date,
-channel_name,
-session_count,	
-total_viewership_minutes,
-unique_viewers,
-avg_session_count,
-avg_session_duration_per_session ,
-avg_session_duration_per_viewer,
-territory_id,
-deal_parent, 
-year, 
-quarter) 
-from (select t.$1, t.$2, to_number(t.$3, 12, 2), to_number(REPLACE(t.$4, ','), 12,2),to_number(REPLACE(t.$5, ','),12,2),
-    to_number(REPLACE(t.$6, ','), 8, 2),
-    to_number(REPLACE(t.$7, ','), 8, 2),
-    to_number(REPLACE(t.$8, ','), 8, 2), 
-    4, 9, 2021, 'q2' from @amagi_viewership t) pattern='.*distro_2021_q2.*' file_format = nosey_viewership 
-ON_ERROR=SKIP_FILE;
-
-
---
-copy into AMAGI_VIEWERSHIP( 
-    date_interval, channel_name,content_id, content_name, SESSION_COUNT, 
-    TOTAL_VIEWERSHIP_MINUTES,UNIQUE_VIEWERS,AVG_SESSION_COUNT,AVG_SESSION_DURATION_PER_SESSION,
-    AVG_SESSION_DURATION_PER_VIEWER, year, quarter
-)
-from (
-    select t.$1, t.$3,t.$4,t.$5, 
-    to_number(REPLACE(t.$6, ','), 12, 2),
-    to_number(REPLACE(t.$7, ','), 12, 2),
-    to_number(REPLACE(t.$8, ','), 12, 2), 
-    to_number(REPLACE(t.$9, ','), 8, 2),
-    to_number(REPLACE(t.$10, ','), 8, 2),
-    to_number(REPLACE(t.$11, ','), 8, 2),
-    2021, 'q3'
-    from @amagi_viewership t
-)
-pattern='.*amagi_q3_21.*' file_format = nosey_viewership 
-ON_ERROR=SKIP_FILE;
-
-
--- Klowd TV
-copy into AMAGI_VIEWERSHIP(view_date,
+-- All channels
+copy into AMAGI_VIEWERSHIP(
+month,
+date_interval,
 channel_name,
 content_id,
 content_name,
 series,
-device,
-country,
 session_count,	
 total_viewership_minutes,
 unique_viewers,
 avg_session_count,
 avg_session_duration_per_session,
 avg_session_duration_per_viewer,
-channel,
-deal_parent, 
 year, 
-quarter) 
-from (select t.$1, t.$2, t.$4, t.$5, t.$6, t.$7, t.$8,
-    to_number(t.$9, 12, 2),
-    to_number(REPLACE(t.$10, ','), 12, 2),
-    to_number(REPLACE(t.$11, ','), 12, 2),
-    to_number(REPLACE(t.$12, ','), 8, 2),
-    to_number(REPLACE(t.$13, ','), 8, 2), 
-    to_number(REPLACE(t.$14, ','), 8, 2), 
-    'Nosey', 12, 2021, 'q3' from @amagi_viewership t) pattern='.*klowd_2021_q3.*' file_format = nosey_viewership 
+quarter,
+filename) 
+from (
+    select t.$1, t.$2, t.$3, t.$4, t.$5, t.$6,
+    to_number(t.$7, 12, 2),
+    to_number(REPLACE(t.$8, ','), 12, 2),
+    to_number(REPLACE(t.$9, ','), 12, 2),
+    to_number(REPLACE(t.$10, ','), 8, 2),
+    to_number(REPLACE(t.$11, ','), 8, 2), 
+    to_number(REPLACE(t.$12, ','), 8, 2), 
+    2021, 'q4', 'amagi_q4_21.csv'  from @distribution_partners t) pattern='.*amagi_q4_21.*' file_format = nosey_viewership 
 ON_ERROR=SKIP_FILE;
+
+
 
 --Update channel_id
 UPDATE AMAGI_VIEWERSHIP t1
