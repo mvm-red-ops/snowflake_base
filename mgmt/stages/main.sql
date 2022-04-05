c6e6a45189ccee5b77a77693ae276db66e6d2507
taylor <tayloryoung@mvmediasales.com>
2022-04-05 16:20:15 -0400

    stage updates

--- mgmt/stages/main.sql
+++ mgmt/stages/main.sql
@@ -1,32 +1,42 @@
-create or replace stage episode_details url='s3://nosey-snowflake/revenue' 
-credentials=(aws_key_id='ENV[AWS_KEY_ID]' aws_secret_key='ENV[AWS_SECRET_ACCESS_KEY]')
-file_format = nosey_viewership;
 
+
+-- distribution_partners
+create or replace stage distribution_partners_viewership url='s3://nosey-snowflake/distribution_partners/viewership' 
+credentials=(aws_key_id='' aws_secret_key='')
+file_format = nosey_viewership;
 
 create or replace stage distribution_partners_revenue url='s3://nosey-snowflake/distribution_partners/revenue' 
-credentials=(aws_key_id='AKIAUASF4VJ5U54QA6ML' aws_secret_key='7j8Q8przvk6drvNsgRNE6gnV39LZB31dqFe20fcS')
+credentials=(aws_key_id='' aws_secret_key='')
 file_format = nosey_viewership;
 
-
-create or replace stage pluto_viewership url='s3://nosey-snowflake/viewership/Pluto' 
-credentials=(aws_key_id='ENV[key_id]' aws_secret_key='ENV[secret_key')
-file_format = nosey_viewership;
-
-
-create or replace stage wurl_viewership url='s3://nosey-snowflake/viewership/wurl' 
-credentials=(aws_key_id='ENV[AWS_KEY_ID]' aws_secret_key='ENV[AWS_SECRET_ACCESS_KEY]')
+create or replace stage distribution_partners_expenses url='s3://nosey-snowflake/distribution_partners/expenses' 
+credentials=(aws_key_id='' aws_secret_key='')
 file_format = nosey_viewership;
 
 
 
-create or replace stage pluto_viewership url='s3://nosey-snowflake/viewership/Pluto' 
-credentials=(aws_key_id='ENV[key_id]' aws_secret_key='ENV[secret_key')
+
+
+
+-- owned_and_operated_
+create or replace stage oo_viewership url='s3://nosey-snowflake/owned_and_operated/viewership' 
+credentials=(aws_key_id='' aws_secret_key='')
 file_format = nosey_viewership;
 
-nosey_assets
+create or replace stage oo_revenue url='s3://nosey-snowflake/owned_and_operated/revenue' 
+credentials=(aws_key_id='' aws_secret_key='')
+file_format = nosey_viewership;
 
-expenses
+create or replace stage oo_expenses url='s3://nosey-snowflake/owned_and_operated/expenses' 
+credentials=(aws_key_id='' aws_secret_key='')
+file_format = nosey_viewership;
 
-nosey_revenue
+create or replace stage oo_ad_data url='s3://nosey-snowflake/owned_and_operated/ad_data' 
+credentials=(aws_key_id='' aws_secret_key='')
+file_format = nosey_viewership;
 
-owned_and_operated
+
+-- other
+create or replace stage nosey_assets url='s3://nosey-snowflake/assets' 
+credentials=(aws_key_id='' aws_secret_key='')
+file_format = nosey_viewership;
