4f96830fffe735e8892a52b044c1351d1e5f4b67
mvm-red-ops <56323037+mvm-red-ops@users.noreply.github.com>
2022-04-18 10:32:37 -0400

    Update b._expenses_upload.sql
    Added directions for AWS, OAO, and POWR

--- OO/step_2/b._expenses_upload.sql
+++ OO/step_2/b._expenses_upload.sql
@@ -4,25 +4,28 @@
 
     -- Nosey Payments/Invoices: https://drive.google.com/drive/u/0/folders/12t56e5-p0ZESxDkxS7hsL8Pu-OMa-EpH
 
-    -- AWS Storage
-    -- OAO Ad Serving
-    -- POWR (Unreel) Platform
+        -- AWS Storage 
+            -- Found in AWS Invoices
+            -- No 'type' for this one
+        -- OAO Ad Serving
+            -- Types are 'adserving' or 'roku'
+            -- 'Adserving' expenses are taken from the total amount of the top section of the invoice
+            -- 'Roku' expenses are taken from the Video Ad Serving fees in the second section
 
+        -- POWR (Unreel) Platform
+            -- Click on POWR (Unreel) Invoices folder
+            -- Types are 'platform' or 'streaming'
+            -- Platform and streaming fees are on separate invoices
 
+        -- create a csv with the following columns: 
+                -- year_month_day, amount, pay_partner, type, quarter, year
+                -- copy values in manually to the correct columns from the invoices 
 
-      -- create a csv with the following columns: 
-            -- year_month_day, amount, pay_partner, type, quarter, year
-            -- copy values in manually to the correct columns from the invoices 
-
-        -- fields to update: 
-            --  filename, year, quarter, pattern
-        copy into expenses( year_month_day, amount, pay_partner, type, quarter, year, filename)
-        from (select t.$1, to_number(REPLACE(REPLACE(t.$2, '$', ''), ','), 12, 2), t.$3, t.$4, t.$5, t.$6,  'expenses_q4_21'
-        from @owned_and_operated t) pattern='.*rev_q4_21.*' file_format = nosey_viewership 
-        ON_ERROR=SKIP_FILE FORCE=TRUE; 
-        
-
-
-        
-
-    
+            -- fields to update: 
+                --  filename, year, quarter, pattern
+            
+-- COPY INTO STATEMENT:
+copy into expenses( year_month_day, amount, pay_partner, type, quarter, year, filename)
+from (select t.$1, to_number(REPLACE(REPLACE(t.$2, '$', ''), ','), 12, 2), t.$3, t.$4, t.$5, t.$6,  'expenses_q4_21'
+from @owned_and_operated t) pattern='.*rev_q4_21.*' file_format = nosey_viewership 
+ON_ERROR=SKIP_FILE FORCE=TRUE; 
