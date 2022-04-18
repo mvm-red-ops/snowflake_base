75b21b0eb7684f7df3183d00f0da8a37f94c6cb6
mvm-red-ops <56323037+mvm-red-ops@users.noreply.github.com>
2022-04-18 11:12:21 -0400

    Fixed typos in statements

--- OO/step_3/1_share_calculations/b._powr.sql
+++ OO/step_3/1_share_calculations/b._powr.sql
@@ -1,13 +1,13 @@
 -- powr viewership
     -- powr viewership by dept and month
-    select sum(watch_time_secods), d.id, d.name, year_month_day, 'powr viewership share' as usage  from powr_viewership p
+    select sum(watch_time_seconds), d.id, d.name, year_month_day, 'powr viewership share' as usage from powr_viewership p
     join nosey_staging.public.departments d on (d.id = p.department_id)
     group by d.name, p.year_month_day, d.id
 
 
-    -- insert viewership by dept adn month
-    insert into  monthly_viewership(tot_viewership, department_id, department_name, year_month_day, usage, quarter )
-    select sum(watch_time_secods), d.id, d.name, year_month_day, 'powr viewership share' as usage, 'q4'  from powr_viewership p
+    -- insert viewership by dept and month
+    insert into  monthly_viewership(tot_viewership, department_id, department_name, year_month_day, usage, quarter)
+    select sum(watch_time_seconds), d.id, d.name, year_month_day, 'powr viewership share' as usage, 'q4' from powr_viewership p
     join nosey_staging.public.departments d on (d.id = p.department_id)
     group by d.name, p.year_month_day, d.id
 
@@ -17,8 +17,8 @@
     set p.share = q.powr_share
     from
     (
-    select p.id as id, ref_id, WATCH_TIME_SECOdS / mv.TOT_VIEWERSHIP as powr_share, p.year_month_day, d.name   from powr_viewership p
-    join monthly_viewership mv on (mv.year_month_day = p.year_month_day ad mv.department_id  = p.department_id)
+    select p.id as id, ref_id, WATCH_TIME_SECONDS / mv.TOT_VIEWERSHIP as powr_share, p.year_month_day, d.name from powr_viewership p
+    join monthly_viewership mv on (mv.year_month_day = p.year_month_day and mv.department_id  = p.department_id)
     join nosey_staging.public.departments d on (d.id = p.department_id)
     where mv.usage = 'powr viewership share'
     ) q
