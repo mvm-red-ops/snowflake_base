40654687c8665f664fd77d5b483b4eb9e8448bad
taylor <tayloryoung@mvmediasales.com>
2022-07-07 16:35:10 -0400

    oo register historical rix

---
+++ mgmt/finance/payment_support.sql
@@ -0,0 +1,24 @@
+-- payment supports are sent out to a few content providers
+-- they send aggregated expenses and revenue for the content provider 
+
+
+update register 
+set payment_suppport_category ='RevShare COS-File Transfer Cost'
+where type like '%PMI%' and payment_suppport_category is null
+		
+alter table register add column  varchar(255)
+		
+alter table registerre
+set payment_suppport_category = 'RevShare COS-Storage Allocation' where title like '%AWS Storage%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-Bandwidth Allocation' where title like '%Brightcove Bandwidth%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-Bandwidth Allocation' where title like '%Brightcove Platform%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-Storage Allocation' where title like '%Brightcove Storage%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category =	'RevShare COS-Storage Bandwidth Allocation' where title like '%Brightcove Storage and Bandwidth%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-AdServing' where title like '%OAO Ad%'	and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-File Transfer Cost' where title like '%PMI File Transfer%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-AdServing' where title like '%POWR (Unreel) Platform%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = "RevShare COS-Closed Captioning Cost" where title like '%Rev.com%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-AdServing ' where title like '%Unreel Platform%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-AdServing' where title like '%WURL Ad Serving%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-Bandwidth Allocation' where title like '%WURL Bandwidth%' and year = 2021 and quarter in ('q3', 'q4')   
+set payment_suppport_category = 'RevShare COS-AdServing ' where title like '%WURL Connector%' and year = 2021 and quarter in ('q3', 'q4')   
--- mgmt/historical_uploads/rev_share.sql
+++ mgmt/historical_uploads/rev_share.sql
@@ -2,28 +2,28 @@
 
 
 -- distribution
-copy into register(year_month_day, department, deal_parent, description, title, amount, content_provider, year, quarter, type, territory_id, channel_id, filename) 
+copy into register(year_month_day, department, title, lineitemtype, type, content_provider, amount, quarter,  year, territory_id, deal_parent, channel_id, filename) 
 from (select 
     t.$1,
     t.$2,
     t.$3,
     t.$4,
     t.$5,
-    REPLACE(REPLACE(t.$6, '$'), ','),
-    t.$7,
+    t.$6,
+    REPLACE(REPLACE(t.$7, '$'), ','),
     t.$8,
     t.$9,
-    t.$10,
-    t.$11,
-    t.$12, 'historical_register.csv'
-        from @distribution_partners_revenue t) pattern='.*historical_register_dist_partners.*' file_format = nosey_viewership 
+     1,
+     25, 
+     8, 
+     'filename.csv'
+        from @distribution_partners_revenue t) pattern='.*filename.*' file_format = nosey_viewership 
         ON_ERROR=SKIP_FILE FORCE=TRUE;
-
         
 
 
 -- Owned and Operated
-copy into register(year_month_day, department, title, type, description, content_provider, rev_share, year, quarter, filename) 
+copy into register(year_month_day, department, title, lineitemtype, type, content_provider, amount, year, quarter, filename) 
 from (select 
     t.$1,
     t.$2,
@@ -40,28 +40,4 @@ from (select
 
 
 
-update expenses set amount = 1111.27 where year_month_day ='20200101' and department = 'All' and title = 'AWS Storage'
-update expenses set amount = 1093.28 where year_month_day = '20200201' and department = 'All' and title = 'AWS Storage'
-update expenses set amount = 1131.29 where year_month_day ='20200301' and 	department = 'All' and title = 'AWS Storage'
-update expenses set amount = 2362.99 where year_month_day ='20200101' and department = 'Mobile/Web' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 1178.60 where year_month_day = '20200201' and department = 'Mobile/Web' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 1203.98 where year_month_day ='20200301' and 	department = 'Mobile/Web' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 10083.71 where year_month_day = '20200201' and department = 'Roku' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 11686.43 where year_month_day ='20200101' and department = 'Roku' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 10597.07 where year_month_day ='20200301' and 	department = 'Roku' and 	title ='Brightcove Bandwidth'
-update expenses set amount = 2416.67 where year_month_day ='20200101' and department = 'All' and title = 'Brightcove Platform'
-update expenses set amount = 2416.67 where year_month_day = '20200201' and department = 'All' and title = 'Brightcove Platform'
-update expenses set amount = 2416.67 where year_month_day ='20200301' and 	department = 'All' and title = 'Brightcove Platform'
-update expenses set amount = 832.83 where year_month_day ='20200101' and department = 'All' and title = 'Brightcove Storage'
-update expenses set amount = 703.63 where year_month_day = '20200201' and department = 'All' and title = 'Brightcove Storage'
-update expenses set amount = 717.84 where year_month_day ='20200301' and 	department = 'All' and title = 'Brightcove Storage'
-update expenses set amount = 312.79 where year_month_day ='20200101' and department = 'Mobile/Web' and 	title = 'OAO Ad Serving'
-update expenses set amount = 1178.99 where year_month_day ='20200101' and Roku and title = 'OAO Ad Serving'
-update expenses set amount = 1987.57 where year_month_day = '20200201' and department = 'Roku' and 	title = 'OAO Ad Serving'
-update expenses set amount = 292.20 where year_month_day = '20200201' and department = 'Mobile/Web' and 	title = 'OAO Ad Serving'
-update expenses set amount = 2265.76 where year_month_day ='20200301' and 	department = 'Roku' and 	title = 'OAO Ad Serving'
-update expenses set amount = 257.22 where year_month_day ='20200301' and 	department = 'Mobile/Web' and 	title = 'OAO Ad Serving'
-update expenses set amount = 5000.00 where year_month_day ='20200101' and department = 'Mobile/Web' and 	title 'Unreel Platform'
-update expenses set amount = 5000.00 where year_month_day = '20200201' and department = 'Mobile/Web' and 	title 'Unreel Platform'
-update expenses set amount = 5000.00 where year_month_day ='20200301' and 	department = 'Mobile/Web' and 	title 'Unreel Platform'
 
